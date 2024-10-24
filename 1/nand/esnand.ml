14bab1667f8316089604e5ebb4f0f81ce68ea724
barbajhonny <giovanniscano52@gmail.com>
2024-10-24 19:19:47 +0200

    esnand

--- 1/nand/esnand.ml
+++ 1/nand/esnand.ml
@@ -1,1 +1,13 @@
+(*Logic port with connectives*)
 let nand a b = not (a && b);;
+
+(*logic port with conditional expression*)
+let nand1 c d = if (c = true && d ==true) then false else true;;
+
+(*Logic port with pattern matching*)
+let nand2 e f =
+  match e, f with
+  | false, false -> true
+  | false, true -> true
+  | true, false -> true
+  |true, true -> false
