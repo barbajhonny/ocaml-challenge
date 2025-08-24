f5bea70065038b4bc17791ab4d870ecf03b25fc1
barbajhonny <giovanniscano52@gmail.com>
2025-08-24 18:21:48 +0200

    Salva esercizi

---
+++ 2/morra/morra.ml
@@ -0,0 +1,9 @@
+type winner = Player | Computer | Tie;;
+let win (p_n,p_guess) =
+  if p_n>= 0 && p_n<=5 && p_guess>=0 && p_guess<=10 then
+    let c_random_number = Random.int 6 in
+    let c_random_guess =  Random.int 11 in
+    if p_n+c_random_number = p_guess then ((c_random_number,c_random_guess),Player) 
+    else if p_n+c_random_number = c_random_guess then ((c_random_number,c_random_guess),Computer)
+    else ((c_random_number,c_random_guess),Tie) 
+  else failwith "out of range";;
