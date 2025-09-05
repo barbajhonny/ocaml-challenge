c3cf25dfc1662d416b28880b88ba2b37b8f7c1ce
barbajhonny <giovanniscano52@gmail.com>
2025-09-05 11:02:26 +0200

    Salva esercizi

--- risiko.ml
+++ risiko.ml
@@ -39,6 +39,8 @@ let no_dup (s: riskstate) : bool =
   List.length territories = List.length unique_territories
 ;;
 
+(*Quesa funzione fa lo stesso di no_dup
+
 let rec no_dup (s:riskstate) = 
   match s with 
   |[] -> true 
@@ -46,8 +48,40 @@ let rec no_dup (s:riskstate) =
   else (no_dup r)  
 ;;
 
+*)
+
 
 let  all_territories  (s:riskstate) =
  List.length s = List.length terrs
-
 ;;
+
+
+(*Questa funzione in pratica cosa fa:
+prende un territorio, un player, un numero di carroarmati e uno stato, confronta questi valori con un determinato stato
+se il territorio è nuovo, quindi non era in possesso da nessun giocatore prima viene direttamente aggiunto allo stato aggioranto, 
+se lo stato invece aveva già un proprietario controlla se il territorio all'interno dello stato è uguale a quello messo, se lo è aggiorna
+la tupla con il nuovo giocatore e il numero di armate, e scorre tutta la lista, nel caso in cui non dovesse essere uguale allora lascia tutto invariato
+
+*)
+let rec update t p n s:riskstate =
+  match s with 
+  | [] -> [(t,p,n)]
+  | (terr,p_vecchio,n_vecchio) :: rest -> if terr = t then (t,p,n)::rest 
+  else (terr,p_vecchio,n_vecchio)::(update t p n rest)
+;;
+
+
+let rec get_armies (t:territory) (s:riskstate) =
+  match s with
+  |[] -> None
+  |(terr,p,n)::rest -> if terr=t then Some (p,n) else get_armies t rest 
+;;
+
+
+let winner ((ta:territory),(va:int)) (td,vd) = if vd >= va then td else ta ;; 
+
+
+(*let lost_armies (a,vala) (b,valb) = *)
+
+
+
