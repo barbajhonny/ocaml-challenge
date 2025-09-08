d6d87150c642bccd8abc6f47ddbb5b0d117b2647
barbajhonny <giovanniscano52@gmail.com>
2025-09-08 15:49:41 +0200

    Salva esercizi

--- 3/knife/knife.ml
+++ 3/knife/knife.ml
@@ -50,4 +50,41 @@ let rec f l1 l2 =
    let select (l1:int list) l2 =
   List.combine l1 l2
     |> List.filter (fun (_,x) -> x)
-    |> List.map (fun (y,_)-> y);;
+    |> List.map (fun (y,_)-> y)
+  ;;
+
+
+
+let f x l = 
+  let occ = List.filter_map(fun y -> if y = x then Some y else None) l in
+  if (List.length occ <= x) then true else false
+;;
+
+
+let foo n l = 
+  let check = List.fold_left (fun acc x -> if x = n && acc<=n then acc+1 else acc ) 0 l in 
+  if check <= n then true else false 
+;;
+
+
+
+let fuu l = 
+  match  l with 
+  |1::rest -> (match List.rev rest with
+                  |1::rest -> List.for_all (fun x -> x=0)  rest
+                  |_ -> false )
+  |_-> false  
+;;
+
+
+let g l = 
+  let head = List.hd l in
+  let tail = List.hd (List.rev l) in
+  
+  if head = 1 && head = tail && List.length l >= 2 then 
+    let f1 = List.filter (fun y -> y <>1) l in
+    let f2 f1 = List.for_all (fun z -> z=0 || [z]=[]) f1 in 
+    
+      if (f2 f1) then true else false 
+else false 
+;;
